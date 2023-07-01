bfab080c526488f0340e665a9458e04596db9e74
Halit Akca <halitakca070@gmail.com>
2023-07-01 16:29:36 +0300

    First Commit

---
+++ app/src/main/java/com/halitakca/artbookkotlin/ArtActivity.kt
@@ -0,0 +1,12 @@
+package com.halitakca.artbookkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArtActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_art)
+    }
+
+}
