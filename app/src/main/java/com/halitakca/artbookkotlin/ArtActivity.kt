be6183ce7d2d125bea5055999ec6bcf5b5169cc4
Halit Akca <halitakca070@gmail.com>
2023-07-01 16:39:23 +0300

    recycler_row is created. View Binding is added to project. activity_art is organised

--- app/src/main/java/com/halitakca/artbookkotlin/ArtActivity.kt
+++ app/src/main/java/com/halitakca/artbookkotlin/ArtActivity.kt
@@ -2,11 +2,24 @@ package com.halitakca.artbookkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.halitakca.artbookkotlin.databinding.ActivityArtBinding
+import com.halitakca.artbookkotlin.databinding.ActivityMainBinding
 
 class ArtActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityArtBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_art)
+        binding = ActivityArtBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
+    fun saveButtonClicked(view: View){
+
+    }
+
+    fun selectImage(view: View){
+
+    }
 }
