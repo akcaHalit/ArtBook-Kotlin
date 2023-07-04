432a4ab4dc747982101c0bfccf5f5e85a19879b8
Halit Akca <halitakca070@gmail.com>
2023-07-04 13:33:59 +0300

    Permission Controls

--- app/src/main/java/com/halitakca/artbookkotlin/ArtActivity.kt
+++ app/src/main/java/com/halitakca/artbookkotlin/ArtActivity.kt
@@ -1,8 +1,14 @@
 package com.halitakca.artbookkotlin
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.halitakca.artbookkotlin.databinding.ActivityArtBinding
 import com.halitakca.artbookkotlin.databinding.ActivityMainBinding
 
@@ -21,5 +27,21 @@ class ArtActivity : AppCompatActivity() {
 
     fun selectImage(view: View){
 
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                // rationale
+                Snackbar.make(view,"Permission needs for Gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission",View.OnClickListener {
+                    // request permission
+                }).show()
+            }else{
+                // request permissions
+            }
+
+        }else{
+            val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            // intent
+        }
+
     }
+
 }
